8c3cb849e08044530ad0680d3152e1b8bc648276
NozomuTsuruta <tuno.0806@icloud.com>
2021-01-29 22:09:32 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [hasSound, setHasSound] = useState(true);
+  useEffect(() => {
+    if ("Notification" in window) {
+      // 通知が許可されていたら早期リターン
+      const permission = Notification.permission;
+      if (permission === "denied" || permission === "granted") {
+        return;
+      }
+      // 通知の許可を求める
+      Notification.requestPermission().then(() => new Notification("テスト"));
+    }
+  }, []);
+
+  const handlePushNotif = () => {
+    if ("Notification" in window) {
+      const notif = new Notification("こんにちは！");
+      notif.addEventListener("show", () => {
+        // 状態によって音の有無を変える
+        if (hasSound) {
+          new Audio("./push.wav").play();
+        }
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handlePushNotif}>PUSH</button>
+      <button onClick={() => setHasSound((prev) => !prev)}>
+        {hasSound ? "音なしにする" : "音ありにする"}
+      </button>
     </div>
   );
 }
